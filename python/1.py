52f6cc3706965e35b2020970876a3cdf95a55fc7
Gerardo Perrucci <centrodph@gmail.com>
2021-03-25 20:45:34 -0300

    python basic

---
+++ python/1.py
@@ -0,0 +1,12 @@
+print("Hello world")
+print(4 + 2)
+
+# read a input from keyword
+
+#es una variable es una asignacion
+num_1 = float(input("Ingrese un numero: ")) 
+num_2 = float(input("Ingrese otro numero: "))
+
+suma = num_1 + num_2
+
+print("el resultado es: ", suma)
