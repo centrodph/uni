11cdb8695c7ffb1794796c5c1e5f730865db6306
Gerardo Perrucci <centrodph@gmail.com>
2021-03-25 20:58:32 -0300

    comments

--- python/1.py
+++ python/1.py
@@ -3,10 +3,17 @@ print(4 + 2)
 
 # read a input from keyword
 
-#es una variable es una asignacion
 num_1 = float(input("Ingrese un numero: ")) 
 num_2 = float(input("Ingrese otro numero: "))
 
 suma = num_1 + num_2
 
 print("el resultado es: ", suma)
+
+"""
+
+Comentarios 
+de varias
+lineas
+
+"""
