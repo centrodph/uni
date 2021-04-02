b98d8645335a9b5243dc29d9b3a4e93028982325
Gerardo Perrucci <centrodph@gmail.com>
2021-04-01 21:31:40 -0300

    mod 4 ej 1

---
+++ python/README.md
@@ -0,0 +1,2 @@
+-  **mod_4_ej_1** Calcular el sueldo de una persona, conociendo la cantidad de horas que trabaja en el mes y el valor de la hora. 
+
---
+++ python/mod_4_ej_1.py
@@ -0,0 +1,10 @@
+print()
+print("Calcular el sueldo de una persona, conociendo la cantidad de horas que trabaja en el mes y el valor de la hora. ")
+print()
+
+cantidad_horas = float(input("Ingrese cantidad de horas: "))
+valor_hora = float(input("Ingrese el valor de hora: "))
+
+print()
+
+print("El sueldo sera $%s" % (cantidad_horas * valor_hora))
