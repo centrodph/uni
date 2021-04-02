79a49bf0ed507f9064894d589edca8d7aeea4527
Gerardo Perrucci <centrodph@gmail.com>
2021-04-01 21:38:20 -0300

    mod_4_ej_2

--- python/README.md
+++ python/README.md
@@ -1,2 +1,9 @@
+
+
 -  **mod_4_ej_1** Calcular el sueldo de una persona, conociendo la cantidad de horas que trabaja en el mes y el valor de la hora. 
+
+
+-  **mod_4_ej_2** Calcular el importe que debe pagar una persona compra una heladera
+de pesos X y por pagar en efectivo le hacen el 10% de descuento ¿Cuánto
+abona?
 
---
+++ python/mod_4_ej_2.py
@@ -0,0 +1,15 @@
+print()
+print("""
+
+Calcular el importe que debe pagar una persona compra una heladera
+de pesos X y por pagar en efectivo le hacen el 10\% de descuento ¿Cuánto
+abona?
+
+""")
+print()
+
+precio_heladera = float(input("Ingrese el precio de la heladera: "))
+
+print()
+
+print("Debera abonar $%s" % (precio_heladera * 0.9))
