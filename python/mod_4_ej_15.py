77d3dab8323d97a7ed027d979d52e442946f2bf3
Gerardo Perrucci <centrodph@gmail.com>
2021-04-08 20:41:33 -0300

    mod 4 ej 15 fix conditions

--- python/mod_4_ej_15.py
+++ python/mod_4_ej_15.py
@@ -9,17 +9,18 @@ no válido” e ingresar un nuevo valor
 
 def mostrarAngulo():
     angulo = int(input("Ingresar la medida del ángulo: "))
-    if angulo == 90:
+    if angulo > 180 or angulo < 0:
+        print("ángulo no válido")
+        mostrarAngulo()
+    elif angulo > 90:
+        print("El angulo es obtuso")
+    elif angulo == 90:
         print("El angulo es recto")
     elif angulo == 180:
         print("El angulo es llano")
     elif angulo == 0:
         print("El angulo es nulo")
-    elif angulo > 90 and angulo < 180:
-        print("El angulo es obtuso")
-    elif angulo < 90 and angulo > 0:
-        print("El angulo es agudo")
     else:
-        print("ángulo no válido")
-        mostrarAngulo()
+        print("El angulo es agudo")
+    
 mostrarAngulo()
