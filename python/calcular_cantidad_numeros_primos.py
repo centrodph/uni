45cc933a0717c6248c71cf19774c9117b2ee5ecb
Gerardo Perrucci <centrodph@gmail.com>
2021-04-29 19:44:12 -0300

    ejercicio primos

---
+++ python/calcular_cantidad_numeros_primos.py
@@ -0,0 +1,25 @@
+print("""
+
+ Ingresar numeros positivos hasta que se ingrese un cero.
+ Se pide calcular la cantidad de numeros primos ingresados.
+
+""")
+
+num = int(input("Ingresar un número: "))
+primos = 0
+
+# validar
+while num < 0:
+    num = int(input("Ingresar un número: "))
+
+while num != 0:
+    mitad = num // 2 + 1
+    cantidad_divisores = 0
+    for i in range(1, mitad, 1):
+        if num % i == 0:
+            cantidad_divisores += 1
+    if cantidad_divisores == 1:
+        primos += 1
+    num = int(input("Ingresar un número: "))
+
+print('cantidad de numeros primos', primos)
