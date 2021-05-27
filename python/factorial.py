ead4f7603c60e69f84c7498257fac888ed5a56a3
Gerardo Perrucci <centrodph@gmail.com>
2021-05-27 19:36:46 -0300

    factorial

---
+++ python/factorial.py
@@ -0,0 +1,22 @@
+print('''
+5! = 5*4*3*2*1
+3! =3*2*1
+0! = 1
+FATORIAL NO ESTA DEFINIDO PARA LOS NEGATIVOS
+Leer 5 número y determinar el factorial de cada uno de ellos
+''')
+
+
+def calcular_factorial(num: int):    
+    fac = 1
+    if num == 0:
+        return 1
+    for i in range(num, 1, -1):
+        fac *= i
+    return fac
+
+for i in range(5):
+    num = int(input("ingrese numero: "))
+    while num < 0:
+        num = int(input("ingrese numero: "))
+    print("El factorial es ", calcular_factorial(num))
