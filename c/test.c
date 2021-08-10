a2489eb0ccf399d25329d79fad593d2711312650
Gerardo Perrucci <centrodph@gmail.com>
2021-08-10 20:41:45 -0300

    update estructura datos 1

---
+++ c/test.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)    
+{
+    // este es un comentario
+    /**
+    comment multi line
+    */
+  printf("Example!\n");
+  
+  return system("read -p 'Press Enter to continue...' var");
+}
