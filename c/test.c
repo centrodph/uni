be369136dcfb2314d0043743705ba8137d216328
Gerardo Perrucci <centrodph@gmail.com>
2021-08-10 21:43:16 -0300

    while/for c

--- c/test.c
+++ c/test.c
@@ -8,6 +8,45 @@ int main(void)
     comment multi line
     */
   printf("Example!\n");
+
+  // o int numero = 8;
+  int numero = 8;
+  int numero2 = 2;
+  float otronumero = 2.555;
+  char c;
+  int counttest;
+
+  printf("Mi numero es: %d %d %d\n", numero, numero, 5);
+
+  printf("Otro formato es: %d %c\n", numero, 'P');
   
-  return system("read -p 'Press Enter to continue...' var");
+
+  printf("Ingrese un numero:\n");
+  scanf("%d", &numero); // Comillas simples para una caracter y comillas dobles string
+
+
+  if ((numero % numero2 == 0 ) && (numero > 0)){
+    printf("el numero es par\n");
+  } else {
+    printf("el numero es impar\n");
+  }
+
+  scanf("%d", &counttest);
+  while(counttest < 5) {
+    counttest++;
+  }
+  printf("despues del loop %d\n", counttest);
+
+
+  int i;
+  for (i = 0; i < 10; i++) {
+    printf("for %d\n", i);
+  }
+
+
+
+
+
+  // return system("pause");
+  // return system("read -p 'Press Enter to continue...' var");
 }
