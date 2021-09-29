91210cc75603cc0e48276fa6abc1ff38c7ab37fd
Gerardo Perrucci <centrodph@gmail.com>
2021-09-28 21:49:56 -0300

    listas

---
+++ c/algoritmos_estructuras/clase_lista_enlazadas.c
@@ -0,0 +1,57 @@
+/**
+
+Dada la siguiente lista simplemente enlazada que contiene la siguiente información:
+    
+    DNI: entero positivo de hasta 8 dígitos.
+    Nombre: cadena de 15 caracteres
+    Tipo de cuenta: carácter (C,E,A)
+    Saldo: real
+    Siguiente: puntero al próximo elemento de la lista.
+    Desarrollar un procedimiento que busque en la lista un nodo cuyo DNI sea igual a uno
+    dado. El mismo devolverá un puntero al nodo hallado o NULL, si no existiera tal nodo
+ **/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct lista
+{
+    int dni;
+    char nombre[15];
+    char cuenta;
+    struct lista *sig;
+} nodo;
+
+void crear(nodo *reg);
+void mostrar(nodo *reg);
+
+int main()
+{
+    nodo *inicial;
+    inicial = (nodo *)malloc(sizeof(nodo));
+    crear(inicial);
+    return 0;
+}
+
+void crear(nodo *reg)
+{
+    printf("Ingrese numero:");
+    scanf("%d", &reg->num);
+    if (reg->num == -1)
+    {
+        reg->sig = NULL;
+    }
+    else
+    {
+        reg->sig = (nodo *)malloc(sizeof(nodo));
+        crear(reg->sig);
+    }
+}
+void mostrar (nodo *reg)
+{
+    if (reg->sig !=NULL)
+	{
+        printf ("%d\n",reg->num);
+        mostrar (reg->sig);
+	}
+	return;
+}
---
+++ c/algoritmos_estructuras/tp_recursividad_06.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *Diseñar un algoritmo recursivo que muestre el número menor de un vector.
+ **/
+int obtener_menor(int v[]);
+
+int main()
+{
+    int vector = {5, 10, 3, 1, 50};
+
+    printf("\n\ncantidad de palabras con mas capicua es %d \n", obtener_menor(vector));
+    return 0;
+}
+
+int obtener_menor(int v[])
+{
+    if()
+}
