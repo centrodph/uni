d23c56ca11ddfa7595dbaac83b34b2bb0d9de4ee
Gerardo Perrucci <centrodph@gmail.com>
2022-03-14 20:47:54 -0300

    with params

--- full-stack/flask/app1.py
+++ full-stack/flask/app1.py
@@ -1,4 +1,4 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 
 app = Flask(__name__)
 
@@ -25,8 +25,18 @@ def get_person(id):
 
     if id in people:
         return people[id]
-    
+
     return Response(f'Person not found: {id}', status=404, mimetype='text/plain')
+
+
+@app.route('/params')
+def get_params():
+    """get params"""
+    print(request.args)
+    params = ''
+    for key, value in request.args.items():
+        params += f'{key}={value} \n'
+    return f'Los parametros son: \n{params}'
 
 
 app.debug = True
--- full-stack/flask/readme.md
+++ full-stack/flask/readme.md
@@ -33,3 +33,19 @@ Date: Mon, 14 Mar 2022 23:25:52 GMT
 
 Person not found%
 ```
+
+
+### con parametros
+
+```
+curl  'http://localhost:5000/params?param1=123&param2=test' -i
+HTTP/1.0 200 OK
+Content-Type: text/html; charset=utf-8
+Content-Length: 46
+Server: Werkzeug/2.0.3 Python/3.9.7
+Date: Mon, 14 Mar 2022 23:47:13 GMT
+
+Los parametros son: 
+param1=123 
+param2=test 
+```
