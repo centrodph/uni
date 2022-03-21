d2ac8377d39aab8d0d15760cae972f919c2c7ebb
Gerardo Perrucci <centrodph@gmail.com>
2022-03-21 19:45:03 -0300

    exceptions

--- full-stack/flask/app1.py
+++ full-stack/flask/app1.py
@@ -11,13 +11,12 @@ people = {
 }
 
 """GET simple"""
+
+
 @app.route('/')
 def index():
     """main page"""
     return "Hello, World!"
-
-
-
 
 
 @app.route('/params')
@@ -29,7 +28,9 @@ def get_params():
         params += f'{key}={value} \n'
     return f'Los parametros son: \n{params}'
 
-## curl http://localhost:5000/people/2 -i
+# curl http://localhost:5000/people/2 -i
+
+
 @app.route('/people/<int:id>')
 def get_person(id):
     """get person"""
@@ -40,7 +41,7 @@ def get_person(id):
     return Response(f'Person not found: {id}', status=404, mimetype='text/plain')
 
 
-## curl http://localhost:5000/people/ -i
+# curl http://localhost:5000/people/ -i
 @app.route('/people/')
 def list_people():
     """get people"""
@@ -49,22 +50,30 @@ def list_people():
 
 
 '''
+Add people
+curl http://localhost:5000/people/ -i -X POST -d '{"nro_doc": "3213323", "nombre":"ejemplo"}' -H "Content-Type: application/json"
+
 curl http://localhost:5000/people/ -i -X POST -d '{"data1": "data1", "data2":"valor2"}' -H "Content-Type: application/json"
 curl http://localhost:5000/people/ -i -X POST -F campo1=valor1 -F campo2=valor2
 '''
+
+
 @app.route('/people/', methods=['POST'])
 def add_person():
     """post params"""
-    print('DATA', request.data)
-    print('FORM', request.form)
-    print('GET JSON', request.get_json())
-    params = ''
-    for key, value in request.args.items():
-        params += f'{key}={value} \n'
-    return f'Los parametros son: \n{params}'
+    # print('DATA', request.data)
+    # print('FORM', request.form)
+    # print('GET JSON', request.get_json())
+    try:
+        data = request.get_json()
+        if not data or not 'nro_doc' in data:
+            return Response('Missing nro_doc', status=400, mimetype='text/plain')
 
+        people[int(data["nro_doc"])] = data["nombre"]
 
-
+        return Response(f'Person added: {data["nro_doc"]}', status=201, mimetype='text/plain')
+    except Exception as e:
+        return Response(f'Error {e}', status=400, mimetype='text/plain')
 
 
 app.debug = True
