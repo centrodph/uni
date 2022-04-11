2b63a6b7f37c21ed31c188cb88fc1c50e82c6847
Gerardo Perrucci <centrodph@gmail.com>
2022-04-11 20:28:33 -0300

    fix controller

--- full-stack/flask/app/app/api/event/controller.py
+++ full-stack/flask/app/app/api/event/controller.py
@@ -20,13 +20,13 @@ from .dto import (
 parser = reqparse.RequestParser()
 parser.add_argument(
     'starts_at',
-    type=inputs.date_from_iso8601,
+    # type=inputs.date_from_iso8601,
     required=True,
     help='returns events that start after the given date',
 )
 parser.add_argument(
     'ends_at',
-    type=inputs.date_from_iso8601,
+    # type=inputs.date_from_iso8601,
     required=True,
     help='returns events that end before the given date',
 )
@@ -58,9 +58,14 @@ class EventList(Resource):
         """
         Returns a list of events
         """
-        # args = parser.parse_args()
-        # events = EventService.get_events(args.starts_at, args.ends_at)
-        # return {'events': events}
+        try:
+            args = parser.parse_args()
+
+            if not args['starts_at'] or not args['ends_at']:
+                abort(400, message='starts_at and ends_at are required')
+
+        except Exception as e:
+            abort(400, message=str(e))
         events = EventService.get_events()
         print(events)
         return jsonify({
