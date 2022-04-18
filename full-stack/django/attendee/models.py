429e2757e6a39d019af4e65d47f3529fd7e8e3f4
Gerardo Perrucci <centrodph@gmail.com>
2022-04-18 19:47:03 -0300

    new field

--- full-stack/django/attendee/models.py
+++ full-stack/django/attendee/models.py
@@ -5,3 +5,4 @@ class Attendee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     gender = models.CharField(max_length=10, default='M')
+    birth_date = models.DateField()
