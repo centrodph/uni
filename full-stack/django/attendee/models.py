aec3899ac438baf45045535b242b55867d5b1a81
Gerardo Perrucci <centrodph@gmail.com>
2022-04-18 20:23:45 -0300

    update events

--- full-stack/django/attendee/models.py
+++ full-stack/django/attendee/models.py
@@ -1,11 +1,27 @@
+from tabnanny import verbose
+from unicodedata import name
 from django.db import models
+
+from event.models.event import Event
 
 # Create your models here.
 class Attendee(models.Model):
+    code = models.CharField(max_length=10, unique=True)
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     gender = models.CharField(max_length=10, default='M')
     birth_date = models.DateField()
 
+    event = models.ForeignKey(
+        Event,
+        related_name='attendees',
+        on_delete=models.DO_NOTHING,
+        null=True,
+    )
+
     def __str__(self) -> str:
         return f"{self.first_name}, {self.last_name}"
+
+    class Meta:
+        verbose_name = 'Asistente'
+        verbose_name_plural = 'Asistentes'    
---
+++ full-stack/django/event/models/event.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+from django.db import models
+
+class Event(models.Model):
+    start_date = models.DateTimeField(default=datetime.now())
+    location = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
+    # description = models.TextField()
+    # end_date = models.DateTimeField()
+    # price = models.DecimalField(max_digits=6, decimal_places=2)
+    # image = models.ImageField(upload_to='events')
+
+    def __str__(self):
+        return self.name
