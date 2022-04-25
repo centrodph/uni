cedf13d1873885200d0160a1509ac8218d17cb4d
Gerardo Perrucci <centrodph@gmail.com>
2022-04-25 20:00:47 -0300

    add swagger

--- full-stack/django/event_project/urls.py
+++ full-stack/django/event_project/urls.py
@@ -14,15 +14,35 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from rest_framework import routers
+from django.views.static import serve
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+from . import settings
 from attendee.urls import router as attendee_router
 
 router = routers.DefaultRouter()
 router.registry.extend(attendee_router.registry)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Full stack Django Event API",
+        default_version='v1',
+        description="API for the Full stack Django Event",
+        contact=openapi.Contact(email="centrodph@gmail.com"),
+    ),
+    public=True,
+)
+
+
+static_urlpatterns = [
+    re_path(r"static/(?P<path>.*)$", serve, {"document_root": "static"}),
+]
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
