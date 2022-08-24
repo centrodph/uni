5fe3146a99325b001cd4380ad4cc7f4fcc43ef9a
Gerardo Perrucci <centrodph@gmail.com>
2022-08-24 19:17:32 -0300

    labo clase 3 update

--- labo_2/clase_03.sql
+++ labo_2/clase_03.sql
@@ -1,10 +1,19 @@
+-- select into es solo para 1 fila
+-- si es mas de 1 o ninguna genera una excepcion
+
+
+
+
 declare
     v_nombre employee.first_name%type;
+    v_salary employee.salary%type;
 begin
-    select first_name
-        into v_nombre
+    select 
+            first_name, salary
+        into 
+            v_nombre, v_salary
         from employee
         where employee_id=7369;
 
-    dbms_output.put_line('nombre: '||v_nombre);
+    dbms_output.put_line('nombre: '||v_nombre||' salario: '||v_salary);
 end;
