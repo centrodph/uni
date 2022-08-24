27ec8fd8528b00ce4f37ba8e161f325771cfa600
Gerardo Perrucci <centrodph@gmail.com>
2022-08-24 19:46:04 -0300

    labo clase 3 nested bloques anonimos

--- labo_2/clase_03.sql
+++ labo_2/clase_03.sql
@@ -20,3 +20,47 @@ exception
     when no_data_found then
         dbms_output.put_line('No existe el empleado');
 end;
+
+
+
+
+
+
+declare
+    v_nombre employee.first_name%type;
+    v_salary employee.salary%type;
+begin
+    select 
+            first_name, salary
+        into 
+            v_nombre, v_salary
+        from employee
+        where department_id = 10;
+
+    dbms_output.put_line('nombre: '||v_nombre||' salario: '||v_salary);
+exception
+    when no_data_found then
+        dbms_output.put_line('No existe el empleado');
+    when too_many_rows then
+        dbms_output.put_line('Existe  mas de un empleado');
+end;
+
+
+declare
+    v_nombre employee.first_name%type;
+    v_salary employee.salary%type;
+begin 
+    begin
+        select 
+                first_name, salary
+            into 
+                v_nombre, v_salary
+            from employee
+            where employee_id = 736;
+    exception
+        when no_data_found then
+        dbms_output.put_line('Exception !');
+    end;
+
+    dbms_output.put_line('fin programa');
+end;
