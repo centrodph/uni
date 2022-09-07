60a3471d73f6a166a18c4269ae8c74d0d7cc793b
Gerardo Perrucci <centrodph@gmail.com>
2022-09-07 19:57:00 -0300

    cursores anidados

--- labo_2/clase_05.sql
+++ labo_2/clase_05.sql
@@ -36,3 +36,37 @@ begin
     listo_emp(v_num);
 end;
 
+
+
+
+
+
+
+---- cursores anidados o encadenados
+
+-- todos los empleados de cada departamento
+create or replace procedure lista_emple_por_depto
+is
+    cursor c_dep is select department_id id, name from department order by department_id;
+    cursor c_emp (p_id_dep number) is select employee_id id, first_name nombre, last_name apellido from employee where department_id = p_id_dep;
+    v_i number :=0;
+begin
+    for r_dep in c_dep loop
+        dbms_output.put_line('Deparamento: '||r_dep.name||' - '||r_dep.id);
+        for r_emp in c_emp (r_dep.id) loop
+            dbms_output.put_line('Empleado: '||r_emp.nombre||' - '||r_emp.apellido);
+            v_i := v_i +1;
+        end loop;
+        if v_i = 0 then
+            dbms_output.put_line('departamento sin empleados');
+        end if;
+        v_i:=0;
+    end loop;
+end lista_emple_por_depto;
+
+declare
+    v_num  number(2) :=1;
+begin
+    lista_emple_por_depto();
+end;
+
